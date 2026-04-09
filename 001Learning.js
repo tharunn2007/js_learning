8843af58b9b019f0f053fd79370f7c802cd5174e
tharunn2007 <dtharunn@gmail.com>
2026-04-09 07:00:52 +0530

    Updates

---
+++ 001Learning.js
@@ -0,0 +1,33 @@
+
+/*EVERYTHING HERE IS BASED OF GOOGLE TABS BASED FOR THIS L1*/
+
+
+/*
+MATH PART
+    *, /  have same precendence and therefore calculate from left to right
+    same goes to +,-
+    brackets can be added for better representation
+
+FLOAT PART PROBLEM
+    0.1+0.2 gives..
+    0.300000000000000004    15 decimal     
+    many programming languages have this porblem since computers have only binary combination unlike humans having intergers from 0 to 9    
+    inaccuary happens only for some value but incase of finance it should absolutuely be avoided
+
+HOW TO AVOID INACCURACY IN FINANCE AND CALCULATING PROBLEMS?
+    calcluate in cents instead of dollars (paisa instead of rupee)
+
+    0.1+0.2 can be written as (20+10)*0.01
+ROUNDING OFF
+    Math.round()
+
+    anything >= x.5 its rounded to x+1
+    or else its x-1
+        vice versa for negative numbers -5.05,-5.5 is all 5 anything grater is -6
+    
+STRING CONCATENATION
+    same as python 'same'+'as'  gives   'sameas'
+
+DATA TYPE CHECKING
+
+*/
